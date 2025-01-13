a2ac870fc708fa53639132af3d9442a29327aa32
Dillon <162556809+dillguill@users.noreply.github.com>
2025-01-13 17:39:31 -0600

    18. Add Media Settings

--- til/urls.py
+++ til/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import include, path
+from django.conf.urls.static import static
 from feed import urls as feed_urls
 
 
@@ -23,4 +25,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('feed.urls', namespace='feed')),
     path('', include('allauth.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
