09000448fa09ad5ac12a9d06d887173513e9ec12
Dillon <162556809+dillguill@users.noreply.github.com>
2025-01-14 19:22:45 -0600

    31. Ajaxing New Posts

--- feed/views.py
+++ feed/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render
 
 from .models import Post
 
@@ -11,6 +12,7 @@ class HomePage(ListView):
     model = Post
     context_object_name = 'posts'
     queryset = Post.objects.all().order_by('-id')[0:30]
+
 
 class PostDetailView(DetailView):
     http_method_names = ['get']
@@ -33,3 +35,20 @@ class CreateNewPost(LoginRequiredMixin, CreateView):
         obj.author = self.request.user
         obj.save()
         return super().form_valid(form)
+
+    def post(self, request, *args, **kwargs):
+
+        post = Post.objects.create(
+            text=request.POST.get("text"),
+            author=request.user,
+        )
+
+        return render(
+            request,
+            "includes/post.html",
+            {
+                "post": post,
+                "show_detail_link": True,
+            },
+            content_type="application/html"
+        )
